0594e433ca1d4bd57eeb1f78e8f46b923d9d2372
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-01-06 14:14:41 -0800

    adds My Passport page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/navbar.js';
+import MyPassport from './components/addedrestaurants/addedrestaurants.js';
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <MyPassport />
+    </>
   );
 }
 
