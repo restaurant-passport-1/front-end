4ae06c77a627830eba0d5a6923ac59fbe9bf9965
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-01-08 08:36:52 -0800

    commit before pulling latest changes

--- src/components/addedrestaurants/PassportRestaurantCard.js
+++ src/components/addedrestaurants/PassportRestaurantCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
+import stamp from './been-here-yellow.svg';
 
 export default function PassportRestaurantCard({ restName, restaurant, removeFromPassport}) {
   return (
     <div className="card passport-card">
+
       <div className="card-image">
         <img src="https://source.unsplash.com/random" alt={`${restaurant.id}`}/>
+        <span className="stamp"><img src={stamp} /></span>
         <span className="card-title">{restName}</span>
       </div>
       <div className="card-action">
