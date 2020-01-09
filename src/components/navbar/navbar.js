63da25604d48987773cf6322dcb1341af3424325
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-01-09 10:36:04 -0800

    switched links in navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,13 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 import 'materialize-css/dist/css/materialize.min.css';
-// import styles from '../../styles/styles.css'
 import HeaderWithLogOut from './logout';
 import SignUpLink from './SignUpLink';
 import SignInLink from './SignInLink';
 
 export default function Navbar({ loggedIn, setLoggedIn }) {
-
   return (
     <nav>
       <div className="nav-wrapper nav_bar">
@@ -15,12 +13,10 @@ export default function Navbar({ loggedIn, setLoggedIn }) {
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li><Link to={"/mypassport"}>My Passport</Link></li>
           <li><a href="#">About</a></li>
-          <li><a href="#">Profile</a></li>
-          
           {loggedIn ? (<li><HeaderWithLogOut setLoggedIn={setLoggedIn} /></li>) : (
             <>
-              <li><SignUpLink /></li>
               <li><SignInLink /></li>
+              <li><SignUpLink /></li>
             </>
           )}
           <li><a href="#"><i className="material-icons">search</i></a></li>
