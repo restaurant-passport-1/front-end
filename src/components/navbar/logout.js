2929c3a851d339e3cc1b34b14493d55683984486
Young Won2 <youngw417@MacBook.local>
2020-01-10 10:43:03 -0800

    final commit2

--- src/components/navbar/logout.js
+++ src/components/navbar/logout.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const HeaderWithLogOut = ({ setLoggedIn }) => {
-	return (
-		<Link className='log-out' to='/login' onClick={() => {
-			localStorage.removeItem('token');
+  return (
+    <Link
+      className="log-out"
+      to="/login"
+      onClick={() => {
+        localStorage.removeItem('token');
 
-			localStorage.removeItem('user_id');
+        localStorage.removeItem('user_id');
+        localStorage.removeItem('city');
 
-
-
-
-			setLoggedIn(false);
-		}}>
-
-			Sign Out
-		</Link>
-	);
+        setLoggedIn(false);
+      }}
+    >
+      Sign Out
+    </Link>
+  );
 };
 
 export default HeaderWithLogOut;
